8770680213d27500f0f321967f0b0d707536878b
András Németh <andrasnemeth@users.noreply.github.com>
2015-08-06 23:12:37 +0200

    Use lambda type deduction in transformations

--- shiftreset.cpp
+++ shiftreset.cpp
@@ -3,6 +3,8 @@
 #include <boost/proto/core.hpp>
 #include <boost/proto/debug.hpp>
 #include <boost/proto/transform.hpp>
+#include <boost/type_traits/function_traits.hpp>
+#include <boost/type_traits/remove_pointer.hpp>
 
 #include <functional>
 #include <iostream>
@@ -74,25 +76,9 @@ struct PlaceholderTransform : boost::proto::transform<PlaceholderTransform> {
             return boost::proto::as_expr(Placeholder());
         }
     };
-
 };
 
 //============================================================================//
-
-// This is not a real grammar, it checks if the expression tree contains a
-// shift-part and replaces the subtree with the result of PlaceholderTransform.
-struct PlaceholderGrammar : boost::proto::or_<
-        boost::proto::when<
-                boost::proto::function<boost::proto::terminal<ResetTag>,
-                        boost::proto::_>,
-                PlaceholderGrammar(boost::proto::_right)>,
-        boost::proto::when<boost::proto::function<
-                boost::proto::terminal<ShiftTag>,
-                boost::proto::terminal<boost::proto::_>>,
-                       PlaceholderTransform>,
-        boost::proto::nary_expr<boost::proto::_,
-                boost::proto::vararg<PlaceholderGrammar>>> {
-};
 
 // This is not a real grammar. It searches for a shift-part of a proto
 // expression tree and extracts the lamda function instance from there. The
@@ -121,6 +107,8 @@ struct ShiftExtractGrammar : boost::proto::or_<
         > {
 };
 
+//============================================================================//
+
 // The context of the evaluation. The most part of the processing is left to
 // proto built-in evaluation function, only the placeholder needs to be
 // substituted to the argument of k.
@@ -141,6 +129,8 @@ public:
 private:
     Arg arg;
 };
+
+//============================================================================//
 
 // This wraps an expression containing a placeholder and provides a callable
 // interface to be converted into an std::function hiding Expr as a kind of
@@ -164,8 +154,178 @@ private:
 
 //============================================================================//
 
+// This is not a real grammar, it checks if the expression tree contains a
+// shift-part and replaces the subtree with the result of PlaceholderTransform.
+struct PlaceholderGrammar : boost::proto::or_<
+        boost::proto::when<
+                boost::proto::function<boost::proto::terminal<ResetTag>,
+                        boost::proto::_>,
+                        PlaceholderGrammar(boost::proto::_right)>,
+        boost::proto::when<boost::proto::function<
+                boost::proto::terminal<ShiftTag>,
+                boost::proto::terminal<boost::proto::_>>,
+                       PlaceholderTransform>,
+        boost::proto::nary_expr<boost::proto::_,
+                boost::proto::vararg<PlaceholderGrammar>>> {
+};
+
+//============================================================================//
+
+// The classes participating in the deduction of the lambda type traits
+// given in the shift part of the expression
+// TODO: DelimitedContinuation might use this deduction
+template<typename T>
+struct MemberFunctionTraits;
+
+template<typename Class, typename R, typename... Args>
+struct MemberFunctionTraits<R(Class::*)(Args...) const>
+        : boost::function_traits<R(Args...)> {
+};
+
+template<typename T>
+struct RemoveConstRef {
+    using type = typename std::remove_reference<
+            typename std::remove_const<T>::type>::type;
+};
+
+template<typename Lambda>
+struct LambdaTraits : MemberFunctionTraits<
+        decltype(&RemoveConstRef<Lambda>::type::operator())> {
+};
+
+//============================================================================//
+
+// template<typename R>
+// class Continuation : std::function<R()> {
+// public:
+//     template<typename Expr>
+//     Continuation(const Expr& expr) {
+//         using Lambda = typename std::result_of<
+//                 ShiftExtractGrammar(Expr)>::type;
+
+//         using Arg = typename LambdaTraits<Lambda>::arg1_type;
+
+//         std::function<R(Arg)> k = ShiftExtractGrammar(expr);
+//         std::function<R(std::function<R(Arg)>)> lambda =
+//                 PlaceholderExpr<
+//                         typename std::result_of<
+//                                 PlaceholderGrammar(Expr)>::type,
+//                         R, Arg>(expr);
+//         callable = std::bind(lambda, k);
+//     }
+
+//     R operator()() {
+//         return callable();
+//     }
+// private:
+//     std::function<R()> callable;
+// };
+
+// This transforms an arbitrary shift-reset expression into a callable
+// object, using the above two grammars and the lambda type traits
+struct DelimitedContinuationTransformation : boost::proto::callable {
+
+    template<typename Signature>
+    struct result;
+
+    template<typename This, typename Expr>
+    struct result<This(Expr)> {
+        // TODO: eliminate duplicaton of type deduction
+        using Lambda = typename std::result_of<
+                ShiftExtractGrammar(Expr)>::type;
+
+        using Arg = typename LambdaTraits<Lambda>::arg1_type;
+
+        using R = typename LambdaTraits<Lambda>::result_type;
+
+        using type = std::function<R()>;
+    };
+
+    template<typename Expr>
+    typename result<DelimitedContinuationTransformation(Expr)>::type
+    operator ()(const Expr& expr) {
+        using Lambda = typename std::result_of<
+                ShiftExtractGrammar(Expr)>::type;
+
+        using Arg = typename LambdaTraits<Lambda>::arg1_type;
+        using R = typename LambdaTraits<Lambda>::result_type;
+
+        std::function<R(Arg)> k = ShiftExtractGrammar(expr);
+        std::function<R(std::function<R(Arg)>)> lambda =
+                PlaceholderExpr<
+                        typename std::result_of<
+                                PlaceholderGrammar(Expr)>::type,
+                        R, Arg>(expr);
+        return std::bind(lambda, k);
+    }
+};
+
+//============================================================================//
+
+template<typename Expr>
+auto makeContinuation(const Expr& expr) {
+        using Lambda = typename std::result_of<
+                ShiftExtractGrammar(Expr)>::type;
+
+        using FunctionArg = typename LambdaTraits<Lambda>::arg1_type;
+        using R = typename LambdaTraits<Lambda>::result_type;
+        using Arg = typename FunctionArg::argument_type;
+
+        std::function<R(std::function<R(Arg)>)> lambda = ShiftExtractGrammar()(
+                expr);
+        std::function<R(Arg)> k =
+                PlaceholderExpr<
+                        typename std::result_of<
+                                PlaceholderGrammar(Expr)>::type,
+                R, Arg>(PlaceholderGrammar()(expr));
+        return std::bind(lambda, k);
+}
+
+// The context of the evaluation. The most part of the processing is left to
+// proto built-in evaluation function, only the placeholder needs to be
+// substituted to the argument of k.
+template<typename R, typename Arg>
+class PlaceholderContext2 : public boost::proto::callable_context<
+        const PlaceholderContext<R, Arg>/*,
+        const boost::proto::null_context*/> {
+public:
+    typedef R result_type;
+
+    explicit PlaceholderContext2(const Arg& arg) : arg(arg) {
+    }
+
+    Arg operator ()(boost::proto::tag::terminal, Placeholder) const {
+        return arg;
+    }
+
+private:
+    Arg arg;
+};
+
+// This wraps an expression containing a placeholder and provides a callable
+// interface to be converted into an std::function hiding Expr as a kind of
+// type erasure.
+template<typename Expr, typename R, typename Arg>
+struct PlaceholderExpr2 {
+
+    // TODO: check if Expr is a valid shift-reset expression
+
+    explicit PlaceholderExpr2(Expr expr) : expr(expr) {
+    }
+
+    R operator ()(Arg arg) {
+        PlaceholderContext<R, Arg> context(arg);
+        return boost::proto::eval(expr, context);
+    }
+
+private:
+    Expr expr;
+};
+
+//============================================================================//
+
 template<typename Sig>
-struct DelimitedContinuation;
+class DelimitedContinuation;
 
 //----------------------------------------------------------------------------//
 
@@ -194,31 +354,30 @@ private:
     std::function<R(std::function<R(Arg)>)> lambda;
  };
 
-// The classes participating in the deduction of the lambda type traits
-// given in the shift part of the expression
-// TODO: DelimitedContinuation might use this deduction
-template<typename T>
-struct FunctionTraits2;
+//============================================================================//
 
-template<typename T, typename R, typename Arg>
-struct FunctionTraits2<R(T::*)(Arg) const> {
+namespace res {
 
-    using Result = R;
-    using Argument = Arg;
+// If a real function is used for 'reset' then the expression until the shift
+// part is eagerly evaluated. This is not the purpose of reset.
+template<typename Expr>
+auto reset(Expr expr) {
+    boost::proto::display_expr(expr);
+}
 
-};
+auto resetTest() {
+    reset(1 + 1 + shift([](std::function<int(int)> k) {
+                        std::cout << k(5) << std::endl;
+                    }) - 1);
+}
 
-template<typename T>
-struct FunctionTraits : FunctionTraits2<decltype(&
-        std::remove_reference<
-                typename std::remove_const<T>::type>::type::operator())> {
-    // TODO: make another metafunction that extracts operator()
-    //       and leave this to strip the const &
-};
+}
 
 //============================================================================//
 
 int main() {
+
+    res::resetTest();
 
     // An example shift-reset expression.
     auto expr = reset(2 + 8 * shift(
@@ -233,7 +392,7 @@ int main() {
     std::function<int(std::function<int(const int&)>)> extractedLambda =
             ShiftExtractGrammar()(expr);
 
-    using LArgument = FunctionTraits<Lambda>::Argument;
+    using LArgument = LambdaTraits<Lambda>::arg1_type;
 
     LArgument a = [](const int& value) {
         return value + 3;
@@ -286,12 +445,22 @@ int main() {
         return value;
     });
 
-    PlaceholderGrammar grammar;
+    auto continuation = reset(1 + shift(
+                    [](std::function<int(int)> k) {
+                        return k(5);
+                    }));
+
+    boost::proto::display_expr(continuation);
+
+    std::cout << "The nerer method: " << makeContinuation(continuation)()
+              << std::endl;
+
+//    PlaceholderGrammar grammar;
     // Calling the grammar will do the transformations on the expression g.
-    auto g = grammar(expr);
+//    auto g = grammar(expr);
     // The reset wrapper part is removed and the shift-enclosed part is replaced
     // with an instance of Placeholder
-    boost::proto::display_expr(g);
+//    boost::proto::display_expr(g);
     // TODO: Do this transformation compile time instead of calling the
     //       grammars directly runtime.
 
@@ -304,18 +473,18 @@ int main() {
     // Using "auto" as the type of delcont2 will leave the type of the
     // reset(...) expression as a pure proto expression. This is one part that
     // could be developed further.
-    DelimitedContinuation<std::function<int(int)>> delcont =
-            reset(2 + shift([](std::function<int(int)> k) { return k(3); }));
+//    DelimitedContinuation<std::function<int(int)>> delcont =
+//            reset(2 + shift([](std::function<int(int)> k) { return k(3); }));
 
     // If we call the operator() on the delimited continuation, it will
     // evaluate the expression and returns the result, 5 in this case.
-    std::cout << "Evaluating the delimited continuation: "
-            << delcont() << std::endl;
+//    std::cout << "Evaluating the delimited continuation: "
+//            << delcont() << std::endl;
 
     // The original "expr" can be converted into a DelimitedContinuation
-    DelimitedContinuation<std::function<int(const int&)>> exprCont = expr;
-    std::cout << "Evaluating the original 'expr' as a delimited continuation: "
-            << exprCont() << std::endl;
+//    DelimitedContinuation<std::function<int(const int&)>> exprCont = expr;
+//    std::cout << "Evaluating the original 'expr' as a delimited continuation: "
+//            << exprCont() << std::endl;
 
     // TODO: solve the type deduction problem that arises from the usage of
     //       lambda expressions.
